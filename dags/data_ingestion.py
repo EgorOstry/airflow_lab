23ea6845b7336982de676cfe9ee0e2674c335ab1
Егор Егор <you@example.com>
2023-09-26 20:14:51 +0300

    Update .gitignore, docker-compose.yaml. Added retries parameter to load_task in data_ingestion dag

---
+++ dags/data_ingestion.py
@@ -0,0 +1,50 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+from datetime import datetime, timedelta
+import os
+from ingestion_script import ingest_data
+
+AIRFLOW_HOME = os.environ.get("AIRFLOW_HOME", "/opt/airflow")
+
+url = 'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2023-01.parquet'
+URL_PREFIX = 'https://d37ci6vzurychx.cloudfront.net/trip-data/'
+URL_TEMPLATE = URL_PREFIX + "yellow_tripdata_{{execution_date.strftime(\'%Y-%m\')}}.parquet"
+OUTPUT_TEMPLATE = AIRFLOW_HOME + "/output_{{execution_date.strftime(\'%Y-%m\')}}.parquet"
+TABLE_TEMPLATE = "yellow_taxi_data"
+
+default_args = {
+    'owner': 'Ostry',
+    'start_date': datetime(2023, 1, 1),
+    'retry_delay': timedelta(seconds=10),
+}
+
+
+workflow = DAG(
+    dag_id='IngestionDag',
+    start_date=datetime(2023,1,1),
+    end_date=datetime(2023,7,1),
+    schedule_interval='0 6 2 * *',
+    max_active_runs=1
+
+)
+
+with workflow:
+
+    curl_task = BashOperator(
+        task_id='curl',
+        bash_command=f'curl -sSL {URL_TEMPLATE} > {OUTPUT_TEMPLATE}'
+    )
+
+    load_task = PythonOperator(
+        task_id='load',
+        python_callable=ingest_data,
+        retries=3,
+        retry_delay=timedelta(seconds=10),
+        op_kwargs=dict(
+            parquet_file=OUTPUT_TEMPLATE,
+            table_name=TABLE_TEMPLATE
+        )
+    )
+
+curl_task >> load_task
